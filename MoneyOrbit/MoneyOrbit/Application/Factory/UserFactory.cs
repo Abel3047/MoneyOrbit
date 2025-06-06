d50241b7065a9a6385549a674465162fc4154399
Abel Tshimbalanga <apexmachine2@gmail.com>
2025-06-06 11:13:20 +0200

    I now also added a second create user method that involves the access level.
    Its filled with errors cause I forgot how to use the case syntax

--- MoneyOrbit/MoneyOrbit/Application/Factory/UserFactory.cs
+++ MoneyOrbit/MoneyOrbit/Application/Factory/UserFactory.cs
@@ -1,6 +1,6 @@
-﻿using MoneyOrbit.Application.Helpers;
-using MoneyOrbit.Application.Interfaces.IApplication.IFactories;
+﻿using MoneyOrbit.Application.Interfaces.IApplication.IFactories;
 using MoneyOrbit.Core.Entities;
+using static MoneyOrbit.Core.Models.Enums;
 
 namespace MoneyOrbit.Application.Factory
 {
@@ -15,6 +15,33 @@ namespace MoneyOrbit.Application.Factory
                 ID = generators.GenerateKey(DateTime.Now), // Generate a new unique ID for the user
                 PasswordHash = encryptedPasswordTuple.Item1, 
                 PasswordSalt = encryptedPasswordTuple.Item2
+            };
+            return _user;
+
+        }
+        public User CreateUser(string _userName, string _firstName, string _lastName, string _password,string userpermission)
+        {
+            string permission = "";
+
+            case (UserPermissions.Customer.ToString()):
+                permission = UserPermissions.Customer.ToString();
+            case (UserPermissions.Administrative.ToString()):
+                permission = UserPermissions.Customer.ToString();
+            case (UserPermissions.Development.ToString()):
+                permission = UserPermissions.Customer.ToString();
+            case _
+                throw new NullReferenceException("You need to provide a valid userpermission");
+
+            Tuple<byte[], byte[]> encryptedPasswordTuple = generators.PasswordEncryptor(_password);
+            User _user = new User()
+            {
+                UserName = _userName,
+                FirstName = _firstName,
+                LastName = _lastName,
+                ID = generators.GenerateKey(DateTime.Now), // Generate a new unique ID for the user
+                PasswordHash = encryptedPasswordTuple.Item1,
+                PasswordSalt = encryptedPasswordTuple.Item2,
+                AccessLevel= permission
             };
             return _user;
 
