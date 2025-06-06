b72764a6517be2e0c95a203935926e4b48e4b3fa
Abel Tshimbalanga <apexmachine2@gmail.com>
2025-06-06 18:57:38 +0200

    -I added a instance of the generator in the firebase Service. -I also edited the applicationServiceExtension file. You can't use the DataService because its an abstract. So I replaced it with the firebase service.
    This means that the DataTrafficControlService is becoming more neccessary. And it will be that that will replace the FirebaseService thats there

--- MoneyOrbit/MoneyOrbit/Application/Extensions/ApplicationServiceExtensions.cs
+++ MoneyOrbit/MoneyOrbit/Application/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿using MoneyOrbit.Application.Interfaces.IServices;
+﻿using MoneyOrbit.Application.Data.Repository;
+using MoneyOrbit.Application.Interfaces.IApplication.IData.IRepository;
+using MoneyOrbit.Application.Interfaces.IEntities;
+using MoneyOrbit.Application.Interfaces.IServices;
 using MoneyOrbit.Infrastructure.Services;
 
 namespace MoneyOrbit.Application.Extensions
@@ -7,15 +10,13 @@ namespace MoneyOrbit.Application.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            services.AddSingleton<IFirebaseService, FirebaseService>();
-
             services.AddScoped<IFirebaseService, FirebaseService>();
-            services.AddScoped<IDataService, DataService>();
-            services.AddScoped<IUserService, UserService>();
 
-            // NOTE: This sets up AutoMapper in our project. It will need a Profile class that defines named configurations
-            // you plan to use. See AutoMapperProfiles for clarification
-            // services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+            //I used FirebaseService here instead of DataService because DataService is an abstract
+            //If you wish to swap this with a different DataService, do so here
+            services.AddScoped<IDataService, FirebaseService>();
+            services.AddScoped<IUserRepository<IUser>, UserRepository>();
+            services.AddScoped<IUserService, UserService>();
 
             return services;
         }
