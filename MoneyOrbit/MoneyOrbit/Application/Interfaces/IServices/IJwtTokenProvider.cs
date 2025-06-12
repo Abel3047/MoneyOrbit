83ab1ace4a56b3fa3cbaf609277de5cf0c74d720
Terrence Titus <terrencekano@gmail.com>
2025-06-12 16:03:52 +0200

    IJwtTokenProvider set up

---
+++ MoneyOrbit/MoneyOrbit/Application/Interfaces/IServices/IJwtTokenProvider.cs
@@ -0,0 +1,9 @@
+﻿using MoneyOrbit.Core.Entities;
+
+namespace MoneyOrbit.Application.Interfaces.IServices
+{
+    public interface IJwtTokenProvider
+    {
+        string Create(User user);
+    }
+}
