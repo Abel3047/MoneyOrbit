516c12ff990d31458a23f24483ec1379eabcada7
cathy <katlego191@gmail.com>
2025-06-14 17:27:51 +0200

    Refactored LoginForm styles and updated component structures for overall improved UI and Dashboard.

--- MoneyOrbit/front-end/moneyorbit/src/Components/LoginForm/LoginForm.jsx
+++ MoneyOrbit/front-end/moneyorbit/src/Components/LoginForm/LoginForm.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./LoginForm.css"; 
-import { FaUser } from "react-icons/fa"; 
-import { FaLock } from "react-icons/fa";
+import { FaUser, FaLock } from "react-icons/fa"; 
 
 
 
@@ -10,16 +9,16 @@ const LoginForm = ({ onLogin }) => {
     return(
         <div className="wrapper">
             <form action= "">
-                <h1>Login</h1>
+                <h1>Sign Up</h1>
                 <div className="input-box">
                     <input type="text"
                      placeholder="Username" required />
-                     <FaUser />
+                     <FaUser className="icon" />
                      </div>
                      <div className="input-box">
                     <input type="text"
                      placeholder="Password" required />
-                     <FaLock />
+                     <FaLock className="icon" />
                      </div>
 
                      <div className="Remember forgot password">
