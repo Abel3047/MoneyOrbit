da5b5008ba420518afabb54896ed5d0b8d459508
Nderitu Ndungo <nndungo@gmail.com>
2025-06-15 09:40:36 +0200

    no time 2

--- MoneyOrbit/front-end/moneyorbit/src/Components/CreateUserForm/CreateUserForm.tsx
+++ MoneyOrbit/front-end/moneyorbit/src/Components/CreateUserForm/CreateUserForm.tsx
@@ -109,7 +109,7 @@ export default function UserCreationForm() {
             </div>
             
             <button type="submit" className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700 transition-colors">
-                Create User
+                Submit
             </button>
         </form>
     );
