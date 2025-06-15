fc82d49959d911dcdd48b35999cb76e217af72c3
Abel Tshimbalanga <apexmachine2@gmail.com>
2025-06-15 03:43:36 +0200

    Put up the linkBankPage template work

---
+++ MoneyOrbit/front-end/moneyorbit/src/pages/LinkBankPage.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import { baseAPIPath } from "../services/baseServices"; // Adjust the import path as necessary
+import LinkBankForm from '../Components/LinkBankForm/LinkBankForm';
+
+interface LinkBankCredentials {
+    //Variables from DTO
+}
+
+export default function LinkBankPage() {
+
+    // 1. State to track the currently active form. Default to 'profile'.
+    const [activeForm, setActiveForm] = useState('linkBank');
+    
+    const navigate = useNavigate();
+
+    // The handleUserRegistration function. This is the "middleware" logic
+    const handleUserRegisteration = async (credentials: LinkBankCredentials) => {
+        // credentials will be an object like { username: 'user123', password: '...' }
+        console.log("OnboardingPage received user registeration credentials:", credentials);
+
+        // This is the mapping step. You convert the data from the form's shape
+        // to the exact shape the API requires.
+        const payload: any = null;
+
+        // --- THIS IS WHERE YOUR API CALL LOGIC GOES ---
+        try {
+            console.log("Sending payload to API:", payload);
+            const response = await axios.post(baseAPIPath + 'User/RegisterUser', payload);
+
+            // If the API call is successful:
+            console.log(response.data);
+            alert('Login successful!');
+
+            // Navigate the user to the dashboard page
+            navigate('/dashboard');
+
+        } catch (error) {
+            console.error('Login failed:', error);
+            alert('Login failed. Please check your credentials.');
+        }
+    };
+
+    // Navigate the user to the dashboard page
+    navigate('/dashboard');
+
+    return (
+        <div className="container mx-auto p-8 max-w-2xl">
+            <h1 className="text-3xl font-bold mb-6">Link your Bank Account</h1>
+
+            {/* 3. Navigation to switch between forms */}
+            <div className="flex border-b mb-6">
+                
+                </div>
+
+           <LinkBankForm />
+        </div>
+    );
+}
--- MoneyOrbit/front-end/moneyorbit/src/pages/OnboardingPage.tsx
+++ MoneyOrbit/front-end/moneyorbit/src/pages/OnboardingPage.tsx
@@ -129,7 +129,7 @@ export default function OnboardingPage() {
 
     return (
         <div className="container mx-auto p-8 max-w-2xl">
-            <h1 className="text-3xl font-bold mb-6">User Settings</h1>
+            <h1 className="text-3xl font-bold mb-6">Onboarding</h1>
 
             {/* 3. Navigation to switch between forms */}
             <div className="flex border-b mb-6">
